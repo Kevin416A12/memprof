b76fccd4cb111ffaa65c693375d8371d4ab49c4d
Kevin416A12 <kevin.c.41683@gmail.com>
2025-09-17 22:32:38 -0600

    Commit inicial
    Va con la libreria, y un main para hacer pruebas,

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "memproflib.h"
+//
+// Created by kevin on 17/9/25.
+//
+int main() {
+
+    std::cout << "Vamos a usar el metodo suma de la biblioteca\n";
+
+    int* ptr;
+    ptr = new int;
+    *ptr = 7;
+    std::cout << "Valor de ptr: " << *ptr << "\n";
+    delete ptr;
+    
+}
